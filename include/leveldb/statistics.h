5588c61f62c5681002b92a4ba7cc988b7b9fae47
foudrer <foudrer@163.com>
2017-04-24 10:54:23 +0800

    add description of how to use

--- include/leveldb/statistics.h
+++ include/leveldb/statistics.h
@@ -2,9 +2,23 @@
 #include <sys/time.h>
 #include <assert.h>
 
+/* How to use:
+
+struct timespec start={0, 0}, end={0, 0};
+
+clock_gettime(CLOCK_REALTIME, &start);
+*program*
+clock_gettime(CLOCK_REALTIME, &end);
+
+double elapsed_select = timespec_diff(&start, &end);
+
+*/
+
 inline double timespec_diff(const struct timespec *start, const struct timespec *end) {
     double r = (end->tv_sec - start->tv_sec) * 1000000000;
     assert(end->tv_nsec > start->tv_nsec);
     r += (end->tv_nsec - start->tv_nsec);
     return (double)(r / 1000000000);
 }
+
+
