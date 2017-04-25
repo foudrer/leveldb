cf4c01d634488d40de940fc4228b1e98921ebcc3
foudrer <foudrer@163.com>
2017-04-25 17:46:49 +0800

    add test script

--- testwr.sh
+++ testwr.sh
@@ -6,7 +6,7 @@ rm -rf $datadir/*
 echo 3 > /proc/sys/vm/drop_caches
 
 #10GB data volume
-out-static/db_bench --benchmarks=fillrandom --value_size=512 --num=20971520 --db=$datadir > $logdata/result_write_512_20971520_opt
+out-static/db_bench --benchmarks=fillrandom --value_size=512 --num=20971520 --db=$datadir > $logdata/result_fillrandom_512_20971520_opt
 
 echo 3 > /proc/sys/vm/drop_caches
-out-static/db_bench --benchmarks=readrandom --value_size=512 --num=20971520 --db=$datadir --use_existing_db=1 > $logdata/result_read_512_20971520_opt
+out-static/db_bench --benchmarks=readrandom --value_size=512 --num=20971520 --db=$datadir --use_existing_db=1 > $logdata/result_readrandom_512_20971520_opt
