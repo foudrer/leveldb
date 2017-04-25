fb7725e7ed27b5e614f3135e3611648c2b80ec96
foudrer <foudrer@163.com>
2017-04-25 08:49:09 +0800

    add time function

--- include/leveldb/statistics.h
+++ include/leveldb/statistics.h
@@ -16,9 +16,36 @@ double elapsed_select = timespec_diff(&start, &end);
 
 inline double timespec_diff(const struct timespec *start, const struct timespec *end) {
     double r = (end->tv_sec - start->tv_sec) * 1000000000;
-    assert(end->tv_nsec > start->tv_nsec);
-    r += (end->tv_nsec - start->tv_nsec);
+
+    if (end->tv_nsec > start->tv_nsec)
+        r += (end->tv_nsec - start->tv_nsec);
+    else
+        r -= (start->tv_usec - end->tv_usec);
+
     return (double)(r / 1000000000);
+}
+
+/* How to use:
+
+struct timeval start, end;
+
+gettimeofday(&start, NULL);
+*program*
+gettimeofday(&end, NULL);
+
+double elapsed_rank = timeval_diff(&start, &end);
+
+*/
+
+inline double timeval_diff(const struct timeval *start, const struct timeval *end) {
+    double r = (end->tv_sec - start->tv_sec) * 1000000;
+
+    if (end->tv_usec > start->tv_usec)
+        r += (end->tv_usec - start->tv_usec);
+    else if (end->tv_usec < start->tv_usec)
+        r -= (start->tv_usec - end->tv_usec);
+
+    return (double)(r / 1000000);
 }
 
 
