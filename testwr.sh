71f345a61781479b91b51812fb8ed818ad38cd7e
foudrer <foudrer@163.com>
2017-04-26 20:07:52 +0800

    add test scripts

--- testwr.sh
+++ testwr.sh
@@ -43,3 +43,33 @@ out-static/db_bench --benchmarks=readrandom --value_size=512 --num=1048576 --db=
 #echo 3 > /proc/sys/vm/drop_caches
 #out-static/db_bench --benchmarks=readrandom --value_size=512 --num=20971520 --db=$datadir --use_existing_db=1 > $logdata/result_readrandom_512_20971520_two_parameter
 
+# three parameter(write and read)
+#rm -rf $datadir/*
+#echo 3 > /proc/sys/vm/drop_caches
+#
+##10GB data volume
+#out-static/db_bench --benchmarks=fillrandom --value_size=512 --num=20971520 --db=$datadir > $logdata/result_fillrandom_512_20971520_three_parameter
+#
+#echo 3 > /proc/sys/vm/drop_caches
+#out-static/db_bench --benchmarks=readrandom --value_size=512 --num=20971520 --db=$datadir --use_existing_db=1 > $logdata/result_readrandom_512_20971520_three_parameter
+
+# four parameter(write and read)
+#rm -rf $datadir/*
+#echo 3 > /proc/sys/vm/drop_caches
+#
+##10GB data volume
+#out-static/db_bench --benchmarks=fillrandom --value_size=512 --num=20971520 --db=$datadir > $logdata/result_fillrandom_512_20971520_four_parameter
+#
+#echo 3 > /proc/sys/vm/drop_caches
+#out-static/db_bench --benchmarks=readrandom --value_size=512 --num=20971520 --db=$datadir --use_existing_db=1 > $logdata/result_readrandom_512_20971520_four_parameter
+
+# five parameter(write and read)
+#rm -rf $datadir/*
+#echo 3 > /proc/sys/vm/drop_caches
+#
+##10GB data volume
+#out-static/db_bench --benchmarks=fillrandom --value_size=512 --num=20971520 --db=$datadir > $logdata/result_fillrandom_512_20971520_five_parameter
+#
+#echo 3 > /proc/sys/vm/drop_caches
+#out-static/db_bench --benchmarks=readrandom --value_size=512 --num=20971520 --db=$datadir --use_existing_db=1 > $logdata/result_readrandom_512_20971520_five_parameter
+
