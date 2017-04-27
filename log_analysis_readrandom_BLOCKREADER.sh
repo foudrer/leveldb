eeb87996d43cb5bb45bc261c31212c13d171e6da
foudrer <foudrer@163.com>
2017-04-27 15:03:33 +0800

    delete some scripts

--- log_analysis_readrandom_BLOCKREADER.sh
+++ log_analysis_readrandom_BLOCKREADER.sh
@@ -1,5 +1,5 @@
-grep "searchfile*" $1 > tmp_file
+grep "cachelookup" $1 > tmp_file
 
-awk 'BEGIN{num=0;total=0;searchfile=0;getvalue=0}{num += 1;searchfile += $2;getvalue += $4;total += $2 + $4}END{print searchfile/num " " getvalue/num " " total/num "\n" searchfile/total*100 " " getvalue/total*100 " " total/total*100}' tmp_file
+awk 'BEGIN{num=0;total=0;cachelookup=0;cachevalue=0;cachereadblock=0;cachereadblock=0;cacheinsert=0;nocachereadblock=0}{num += 1;cachelookup += $2;cachevalue += $4;cachereadblock += $6;cacheinsert += $8;nocachereadblock += $10;total += $2 + $4 + $6 + $8 + $10}END{print cachelookup/num " " cachevalue/num " " cachereadblock/num " " cacheinsert/num " " nocachereadblock/num "\n" cachelookup/total*100 " " cachevalue/total*100 " " cachereadblock/total*100 " " cacheinsert/total*100 " " nocachereadblock/total*100 " " total/total*100}' tmp_file
 
 rm -rf tmp_file
--- log_analysis_readrandom_READBLOCK.sh
+++ log_analysis_readrandom_READBLOCK.sh
@@ -1,5 +1,5 @@
-grep "searchfile*" $1 > tmp_file
+grep "fileread" $1 > tmp_file
 
-awk 'BEGIN{num=0;total=0;searchfile=0;getvalue=0}{num += 1;searchfile += $2;getvalue += $4;total += $2 + $4}END{print searchfile/num " " getvalue/num " " total/num "\n" searchfile/total*100 " " getvalue/total*100 " " total/total*100}' tmp_file
+awk 'BEGIN{num=0;total=0;fileread=0;filecrc=0;uncompress=0}{num += 1;fileread += $2;filecrc += $4;uncompress += $6;total += $2 + $4 + $6}END{print fileread/num " " filecrc/num " " uncompress/num " " total/num "\n" fileread/total*100 " " filecrc/total*100 " " uncompress/total*100 " " total/total*100}' tmp_file
 
 rm -rf tmp_file
