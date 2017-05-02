39bce2222aab267e074a17e6e7832fcf0387f666
foudrer <foudrer@163.com>
2017-05-02 14:40:32 +0800

    fix findtable test

--- test_read_performance.sh
+++ test_read_performance.sh
@@ -1,7 +1,11 @@
 datadir=/mnt/testdata1
 logdata=/mnt/logdata
 
-# test log
-
+rm -rf $datadir/*
 echo 3 > /proc/sys/vm/drop_caches
-out-static/db_bench --benchmarks=readrandom --value_size=512 --num=10485760 --db=$datadir --use_existing_db=1 > $logdata/result_readrandom_log
+
+out-static/db_bench --benchmarks=fillrandom --value_size=512 --num=20971520 --db=$datadir
+
+# test log
+echo 3 > /proc/sys/vm/drop_caches
+out-static/db_bench --benchmarks=readrandom --value_size=512 --num=20971520 --db=$datadir --use_existing_db=1 > $logdata/result_readrandom_512_20971520_six
